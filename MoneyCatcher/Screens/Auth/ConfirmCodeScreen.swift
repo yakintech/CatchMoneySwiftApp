38a658f5418d3f5935913b81e64f29b9436cfb7f
alperen <146393353+alperennad@users.noreply.github.com>
2024-09-19 18:21:25 +0300

    otp screen | added

--- MoneyCatcher/Screens/Auth/ConfirmCodeScreen.swift
+++ MoneyCatcher/Screens/Auth/ConfirmCodeScreen.swift
@@ -5,16 +5,85 @@
 //  Created by Çağatay Yıldız on 18.09.2024.
 //
 
+//4 tane karecik olacak
+//kareler 1 deger alabilecek
+//kareler rakam alacak
+
 import SwiftUI
 
 struct ConfirmCodeScreen: View {
+    @State private var code1: String = ""
+    @State private var code2: String = ""
+    @State private var code3: String = ""
+    @State private var code4: String = ""
+    
+    
     var body: some View {
+        Text("VERİFİCATİON")
+            .font(.title)
+            .fontWeight(.bold)
+        
+            .padding()
+            .padding()
+            .padding()
+        
+        Text("Enter the 4-digit code sent to your email")
+            //.padding()
+            .foregroundColor(.black)
         VStack{
             
+            HStack{
+                TextField("", text: $code1)
+                    .padding()
+                    .multilineTextAlignment(.center)
+                    .frame(height: 50)
+                    .background(Color.gray.opacity(0.2))
+                    .cornerRadius(10)
+                    .frame(width:50)
+                
+                TextField("", text: $code2)
+                    .padding()
+                    .multilineTextAlignment(.center)
+                    .frame(height: 50)
+                    .background(Color.gray.opacity(0.2))
+                    .cornerRadius(10)
+                    .frame(width:50)
+                
+                TextField("", text: $code3)
+                    .padding()
+                    .multilineTextAlignment(.center)
+                    .frame(height: 50)
+                    .background(Color.gray.opacity(0.2))
+                    .cornerRadius(10)
+                    .frame(width:50)
+                
+                TextField("", text: $code4)
+                    .padding()
+                    .multilineTextAlignment(.center)
+                    .frame(height: 50)
+                    .background(Color.gray.opacity(0.2))
+                    .cornerRadius(10)
+                    .frame(width:50)
+            }
+            
+            .padding()
+            Button(action :{
+                print("Done.")
+            })    {
+                Text("Verify")
+                    .font(.title3)
+                    .frame(width: 230)
+                    .frame(height: 35)
+                    .background(Color.orange)
+                    .foregroundColor(.white)
+                    .cornerRadius(20)
+            }
         }
+
     }
 }
 
-#Preview {
+#Preview
+{
     ConfirmCodeScreen()
 }
