43edc914d339a7ad53376a98f35ff2d371e50c65
cagatay.yildiz <cagatay@pixupplay.com>
2024-09-26 19:02:11 +0300

    userdata

---
+++ MoneyCatcher/Screens/Expense/AddExpense.swift
@@ -0,0 +1,18 @@
+//
+//  AddExpense.swift
+//  MoneyCatcher
+//
+//  Created by Çağatay Yıldız on 24.09.2024.
+//
+
+import SwiftUI
+
+struct AddExpense: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddExpense()
+}
